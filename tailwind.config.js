b8288abed85c7cf34ab3ef84e46f48dff6eda85d
Darian Nocera <darnocer@gmail.com>
2024-04-23 12:43:54 -0600

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.emerald,
+        secondary: colors.cyan,
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        h1: ["3rem", { lineHeight: "1.2" }],
+        h2: ["2.25rem", { lineHeight: "1.3" }],
+        h3: ["1.875rem", { lineHeight: "1.4" }],
+        h4: ["1.5rem", { lineHeight: "1.5" }],
+        h5: ["1.25rem", { lineHeight: "1.6" }],
+        h6: ["1.125rem", { lineHeight: "1.7" }],
+      },
+    },
+  },
+  plugins: [],
+};
