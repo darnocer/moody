b9b03f9a58488174c3f6f42738c789b9e23bfd72
Darian Nocera <darnocer@gmail.com>
2024-04-26 09:27:45 -0600

    add mood entries content data

---
+++ data/moodEntries.js
@@ -0,0 +1,23 @@
+const moodEntries = {
+  heading: "New Mood Entry",
+  mood: {
+    heading: "Select your mood",
+  },
+  journal: {
+    linkTextBefore: "Add note",
+    linkTextAfter: "Edit note",
+    button: "Save",
+  },
+  influences: {
+    heading: "What's influencing your current mood?",
+  },
+  feelings: {
+    heading: "Feeling anything else?",
+  },
+  submit: {
+    button: "Complete Checkin",
+    loadingText: "Submitting...",
+    successMessage: "Mood entry saved successfully!",
+  },
+};
+module.exports = moodEntries;
